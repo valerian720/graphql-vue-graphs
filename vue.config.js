27c8a10d06a0d864f0cec384bd110461a2d86bf3
Валериан Воротников <valerian7202@gmail.com>
2023-06-04 10:03:40 +0300

    autofix + script

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  // auto fix on save
+  chainWebpack: (config) => {
+    config.plugin("eslint").tap((args) => {
+      args[0].fix = true;
+      return args;
+    });
+  },
 });
